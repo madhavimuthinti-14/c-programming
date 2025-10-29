28a92d5fde0612b5ad3d9922be18ea39cf9fd67c
madhavimuthinti-14 <madhavimuthinti@gmail.com>
2025-10-29 14:40:12 +0530

    Add files via upload

---
+++ sum of two numbers.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	int i=5,j=5,k;
+	k=i+j;
+	printf("sum=%d",k);
+	
+}
---
+++ unary.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int x=10,p,q,r,s;
+	p=++x;
+	printf("p=%d",p);
+	q=x--;
+	printf("\n q=%d",r);
+	r=x++;
+	printf("\n r=%d",r);
+	s=--x;
+	printf("\n s=%d",s);
+	printf("\n x=%d",x);
+	return 0;
+	
+}
